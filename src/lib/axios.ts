433395e96da73b87b8981716489da6d25c7ff7a2
akshay <akshaymugale1@gnmail.com>
2025-06-13 19:45:05 +0530

    baseUrl set

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-
 // Get Item from Local storage for dynamic ... save while login
 const TOKEN = "653002727bac82324277efbb6279fcf97683048e44a7a839";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", 
+  //   baseURL: "http://localhost:3000",
+  baseURL: "https://marathon.lockated.com",
+
   headers: {
     "Content-Type": "application/json",
   },
   params: {
-    token: TOKEN, 
+    token: TOKEN,
   },
 });
 
